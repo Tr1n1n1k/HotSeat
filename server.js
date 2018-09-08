be5fdc8cc6fcb144e2e9986e9550823fd88549fe
Anthony Yin <anthony923x@gmail.com>
2018-09-08 12:15:34 -0400

    tyoe error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.sendFile(path.john(__dirname, 'text.html'));
+  res.sendFile(path.join(__dirname, 'text.html'));
 });
 
 app.listen(PORT, function() {
